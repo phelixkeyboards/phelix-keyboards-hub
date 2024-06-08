6a853331ebb9c5ce4238146b168a50a1e5a952a0
Alfayed Baksh <baksha97@gmail.com>
2024-06-08 13:45:14 -0400

    ts electron + electron force rebuild

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,31 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+
+interface ElectronAPI {
+  listPKDevices: () => void;
+  onHidPKDevices: (callback: (devices: any) => void) => void;
+  listHidDevices: () => void;
+  onHidDevices: (callback: (devices: any) => void) => void;
+  sendTime: () => void;
+}
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  listPKDevices: (): void => {
+    ipcRenderer.send('list-pk-devices');
+  },
+  onHidPKDevices: (callback: (devices: any) => void): void => {
+    ipcRenderer.on('hid-pk-devices', (event: IpcRendererEvent, devices: any) => {
+      callback(devices);
+    });
+  },
+  listHidDevices: (): void => {
+    ipcRenderer.send('list-hid-devices');
+  },
+  onHidDevices: (callback: (devices: any) => void): void => {
+    ipcRenderer.on('hid-devices', (event: IpcRendererEvent, devices: any) => {
+      callback(devices);
+    });
+  },
+  sendTime: (): void => {
+    ipcRenderer.send('send-time');
+  },
+} as ElectronAPI);
--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -17,4 +17,7 @@ export const mainConfig: Configuration = {
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
   },
+  externals: {
+    "node-hid" : 'commonjs node-hid'
+  },
 };
